d2a7d1f33ecd4669422046603bbc091f3fba70af
Nicholas Kalpakis <nkalpakis21@gmail.com>
2023-02-28 21:15:05 -0800

    Add feed.

---
+++ components/input/Input.tsx
@@ -0,0 +1,23 @@
+import { FaceSmileIcon, PhotoIcon } from '@heroicons/react/24/outline'
+import Image from 'next/image'
+import { useState } from 'react'
+import ProfilePic from '../../public/profile_picture.jpeg'
+export default function Input () {
+    const [tweetText, setTweetText] = useState('');
+    console.log(tweetText);
+    return (
+        <div className="flex w-full space-x-4 p-4">
+            <div>
+                <Image src={ProfilePic} alt="profile-pic" height={60} width={60} className="rounded-full" />
+            </div>
+            <div className="w-full divide-y">
+                <textarea value={tweetText} onChange={(e) => setTweetText(e.target.value)} className="w-full focus:outline-none" name="" id="" cols={2} rows={2} placeholder="What's happening?"/>
+                <div className="flex mt-2 pt-2.5">
+                    <PhotoIcon className="h-6 w-6 text-sky-500 hover:bg-sky-100"/>
+                    <FaceSmileIcon className="h-6 w-6 text-sky-500 hover:bg-sky-100"/>
+                    <button disabled={true} className={`ml-auto bg-blue-400 text-white rounded-full w-20 h-8 font-bold shadow-md hover:brightness-95`}>Tweet</button>
+                </div>
+            </div>
+        </div>
+    )
+}
